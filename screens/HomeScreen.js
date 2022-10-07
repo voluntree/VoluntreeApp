e2c2989c16becf2c36517b8e9fdaf3210cee01c9
catalinm00 <catalin.marian123@gmail.com>
2022-10-07 15:48:25 +0200

    w

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,35 +1,39 @@
-import { View, Text, TouchableOpacity, TextInput} from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import MapView from 'react-native-maps';
-import { TailwindProvider } from 'tailwindcss-react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MapView from "react-native-maps";
+import { TailwindProvider } from "tailwindcss-react-native";
 import { doc, setDoc } from "firebase/firestore";
-import {db} from '../utils/firebase';
-import { useState } from 'react';
-import TarjetaDeActividad from '../components/TarjetaDeActividad';
+import { db } from "../utils/firebase";
+import { useState } from "react";
+import TarjetaDeActividad from "../components/TarjetaDeActividad";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
-      navigation.setOptions({
-          headerShown: false,
-      });
-  }, [])
-
-  
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
     <TailwindProvider>
-      <SafeAreaView className = "bg-white h-full items-center pt-5">
-        <TarjetaDeActividad/>
+      <SafeAreaView className="bg-white h-full items-center pt-5">
+        <ScrollView>
+          <View><TarjetaDeActividad /></View>
+          
+        </ScrollView>
       </SafeAreaView>
     </TailwindProvider>
   );
-}
+};
 
-
-
-
-export default HomeScreen
+export default HomeScreen;
