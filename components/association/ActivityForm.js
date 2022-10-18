9b79f32d2b6d91428742d164c55c03eb23eaf72d
zhehao <zhxq09.80@gmail.com>
2022-10-18 23:04:03 +0200

    guardar en la BBDD

--- components/association/ActivityForm.js
+++ components/association/ActivityForm.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { ScrollView, StyleSheet, Button, TextInput, View, Text } from "react-native";
+import { ScrollView, StyleSheet, Button, TextInput, View, Text, TouchableOpacity } from "react-native";
 import { Formik } from "formik";
+
+import service, { saveActivity } from '../../service/service';
 
 const ActivityForm = () => {
     const styles = StyleSheet.create({
@@ -8,40 +10,89 @@ const ActivityForm = () => {
             borderWidth: 1,
             borderColor: "#ddd",
             padding: 10,
-            fontSize: 18,
+            fontSize: 12,
             borderRadius: 10,
-            marginBottom: 10,
+            marginBottom: 20,
+        },
+        description: {
+            borderWidth: 1,
+            borderColor: "#ddd",
+            padding: 10,
+            fontSize: 12,
+            borderRadius: 10,
+            marginBottom: 20,
+        },
+        horizontal: {
+            flexDirection: "row",
+        },
+        imageButton: {
+            width: 162,
+            height: 162,
+            backgroundColor: '#d3d3d3',
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
         }
     })
     
     return (
-        <ScrollView style={{padding: 20, paddingTop: 60,}}>
+        <ScrollView style={{padding: 20, paddingTop: 100,}}>
             <Formik
-                initialValues={{ title: '', body: '', rating: '' }}
+                initialValues={{ title: '', type: '', participants: '', duration: '', description: ''}}
                 onSubmit={(values) => {
-                    console.log(values);
+                    saveActivity(values);
                 }}
             >
                 {(props) => (
                     <View>
-                        <TextInput
-                            style={styles.input}
-                            placeholder='Título de la actividad'
-                            onChangeText={props.handleChange('title')}
-                            value={props.values.title}
-                        />
+                        <View style={styles.horizontal}>
+                            <View style={{width: 180, marginRight: 10}}>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder='Título de la actividad'
+                                    onChangeText={props.handleChange('title')}
+                                    value={props.values.title}
+                                />
 
-                        <TextInput
-                            style={styles.input}
-                            multiline
-                            placeholder='Tipo'
-                            onChangeText={props.handleChange('body')}
-                            value={props.values.body}
-                            
-                        />
+                                <TextInput
+                                    style={styles.input}
+                                    multiline
+                                    placeholder='Tipo'
+                                    onChangeText={props.handleChange('type')}
+                                    value={props.values.type}
+                                    
+                                />
+                                <View style={styles.horizontal}>
+                                    <TextInput
+                                        style={[styles.input, {marginRight: 10, width: 95}]}
+                                        placeholder='Número de participantes'
+                                        textAlignVertical='top'
+                                        onChangeText={props.handleChange('participants')}
+                                        value={props.values.participants}
+                                        keyboardType='numeric'
+                                    />
 
+                                    <TextInput
+                                        style={[styles.input, {width: 75}]}
+                                        placeholder='Duración'
+                                        textAlignVertical='top'
+                                        onChangeText={props.handleChange('duration')}
+                                        value={props.values.duration}
+                                        keyboardType='numeric'
+                                    />
+                                </View>
+                            </View>
+                            <View>
+                                <TouchableOpacity>
+                                    <View style={styles.imageButton}>
+                                        <Text style={{fontSize: 20, color: '#fff'}}>+</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        
                         <TextInput
-                            style={styles.input}
+                            style={styles.description}
                             placeholder='Descripción'
                             multiline={true}
                             numberOfLines={4}
@@ -49,25 +100,6 @@ const ActivityForm = () => {
                             onChangeText={props.handleChange('description')}
                             value={props.values.description}
                         />
-
-                        <TextInput
-                            style={styles.input}
-                            placeholder='Número de participantes'
-                            textAlignVertical='top'
-                            onChangeText={props.handleChange('participants')}
-                            value={props.values.participants}
-                            keyboardType='numeric'
-                        />
-
-                        <TextInput
-                            style={styles.input}
-                            placeholder='Duración'
-                            textAlignVertical='top'
-                            onChangeText={props.handleChange('duration')}
-                            value={props.values.duration}
-                            keyboardType='numeric'
-                        />
-
                         <Button title='Crear' color='maroon' onPress={props.handleSubmit} />
                     </View>
                 )}
--- service/service.js
+++ service/service.js
@@ -8,11 +8,14 @@ import {
   doc,
   onSnapshot,
   firestore,
+  setDoc,
 } from "firebase/firestore";
+import { FirebaseError } from "firebase/app";
 
 const actividadesRef = collection(db, "actividades");
 const voluntarioRef = collection(db, "voluntarios");
 
+//#region Actividades
 export async function getAllActivities() {
   const activities = [];
   try {
@@ -40,3 +43,16 @@ export async function getActivityById(id) {
     console.log(error);
   }
 }
+
+// Guarda una actividad en la base de datos
+export async function saveActivity(activity) {
+  try {
+    const docRef = doc(db, "actividades", activity.title);
+    await setDoc(docRef, activity);
+    console.log('Actividad guardada');
+  } catch (error) {
+    console.error('Error al guardar la actividad', error);
+  }
+}
+
+//#endregion
