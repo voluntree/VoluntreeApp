f7a49ce00b20d04119444abd20e2800384347309
voluntree <voluntree05@gmail.com>
2022-10-20 12:25:48 +0200

    Acabada interfaz Filtrar

--- components/Filtro.js
+++ components/Filtro.js
@@ -1,14 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacityBase, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Image } from 'react-native-elements'
 
 const Filtro = (props) => {
   return (
-    <View className = "items-center px-2.5">
-        <Image className = "w-12 h-12 rounded-full" source = {props.imagen} />
-        <Text className = "text-[12px] font-medium">{props.texto}</Text>
-    </View>
-  )
+    <TouchableOpacity>
+      <View className="px-4 pb-1">
+        <View className="items-center w-16 h-16">
+          <Image className="w-12 h-12 rounded-full" source={props.imagen} />
+          <Text className="text-[10px] font-semibold">{props.texto}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 export default Filtro
