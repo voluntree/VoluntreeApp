166c185ba3442b6492615dc9639840674096ef37
ZheHao <zhxq09.80@gmail.com>
2022-11-14 00:12:56 +0100

    datetimepicker

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -18,7 +18,7 @@ import QRGenerator from './../screens/association/QRGenerator';
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const AppNavigator = () => (
-    <Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
+    <Navigator screenOptions={{headerShown: false}} initialRouteName="AssociationHome">
         <Screen name = 'Login' component ={Login}></Screen>
         <Screen name='LoginScreen' component={LoginScreen}></Screen>
         <Screen name='UserHome' component={TabNavigator}></Screen>
