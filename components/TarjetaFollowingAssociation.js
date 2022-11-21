8663fda6a01b862eba28472d66301e76d4b0041b
adsomar <adrimiramar311@gmail.com>
2022-11-21 12:57:36 +0100

    Imagen lista asociaciones seguidas

--- components/TarjetaFollowingAssociation.js
+++ components/TarjetaFollowingAssociation.js
@@ -1,15 +1,27 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
-import { Icon } from "react-native-elements";
+import { Icon, Image } from "react-native-elements";
 import { useEffect } from "react";
 import { theme } from "../tailwind.config";
 import { unfollowAsociation } from "../service/service";
+import { getDownloadURL, ref} from "firebase/storage";
+import { storage } from "../utils/firebase";
 
 const TarjetaFollowingAssociation = (props) => {
+  const [imagen, setImagen] = useState()
+  
+  useEffect (() => {
+    getDownloadURL(ref(storage,"gs://voluntreepin.appspot.com/" + props.asociacion.fotoPerfil))
+      .then((path) => {
+        setImagen(path);
+      });
+  }, [])
+  
   return (
         <View className = "bg-blanco flex-row justify-between items-center p-2 rounded-md mb-2">
           <View className = "flex-row items-center space-x-2">
-            <View className="border-solid rounded-full bg-bottomTabs w-12 h-12"></View>
+            <Image className="border-solid border-2 border-[#ececec] rounded-full w-12 h-12" 
+                   source={{uri: imagen}}/>
             <Text className="font-bold text-lg">{props.asociacion.nombre}</Text>
             </View>   
           <TouchableOpacity onPress={() => {unfollowAsociation(props.usuario, props.asociacion.nombre)}}>
