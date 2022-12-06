c63c596eb52ec607041a2e39e9ad2f5314b603ba
adsomar <adrimiramar311@gmail.com>
2022-12-06 13:40:10 +0100

    Mas test

--- test/service.test.js
+++ test/service.test.js
@@ -1,14 +1,13 @@
 import {
   getAllActivities,
-  
-        getActivityById,
+  getActivityById,
   followAsociation,
   getImageDownloadURL,
   deleteUserData,
   getAsociationByID, 
-        getActivityByTitle, 
-        inscribirUsuarioEnActividad,
-        desapuntarseDeActividad,
+  getActivityByTitle, 
+  inscribirUsuarioEnActividad,
+  desapuntarseDeActividad,
         createActivity,
         deleteActivity,
         updateActivity
@@ -22,31 +21,21 @@ import { text } from "stream/consumers";
 import { Timestamp } from "@firebase/firestore";
 import { async } from "q";
 
-test("obtiene todas la actividades de la base de datos", async () => {
-  expect((await getAllActivities()).length).toBeGreaterThan(2);
-});
-
-test("obtiene una actividad con el id", async () => {
-  expect((await getActivityById("Apoyo Escolar a Jovenes de ESO")).titulo).toBe(
-    "Apoyo Escolar a Jovenes de ESO"
-  );
-});
-
-const urls = [
-  {
-    url: "https://firebasestorage.googleapis.com/v0/b/voluntreepin.appspot.com/o/productos%2FcantimploraEco.png?alt=media&token=8e67418a-2e75-4ad5-87d4-7da0667fecf5",
-  },
-  {
-    url: "https://firebasestorage.googleapis.com/v0/b/voluntreepin.appspot.com/o/productos%2FcantimploraEco2.png?alt=media&token=c66feab4-777b-4ea9-8429-763f93830282",
-  },
-  {
-    url: "https://firebasestorage.googleapis.com/v0/b/voluntreepin.appspot.com/o/productos%2Fecobag.png?alt=media&token=b8d82ac5-6bfd-4a14-97dd-6a50491bc2c3",
-  },
-];
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+const urls = [
+    {
+      url: "https://firebasestorage.googleapis.com/v0/b/voluntreepin.appspot.com/o/productos%2FcantimploraEco.png?alt=media&token=8e67418a-2e75-4ad5-87d4-7da0667fecf5",
+    },
+    {
+      url: "https://firebasestorage.googleapis.com/v0/b/voluntreepin.appspot.com/o/productos%2FcantimploraEco2.png?alt=media&token=c66feab4-777b-4ea9-8429-763f93830282",
+    },
+    {
+      url: "https://firebasestorage.googleapis.com/v0/b/voluntreepin.appspot.com/o/productos%2Fecobag.png?alt=media&token=b8d82ac5-6bfd-4a14-97dd-6a50491bc2c3",
+    },
+  ];
 
 test("descarga de las imagenes correctamente", async () => {
   expect(
@@ -57,7 +46,7 @@ test("descarga de las imagenes correctamente", async () => {
 });
 
 test("compara fecha mas", async () => {
-  const act1 = await getActivityById("Apoyo Escolar a Jovenes de ESO");
+  const act1 = await getActivityById("Plantación en la Vallesa");
   const act2 = await getActivityById("Pasear perros por la huerta");
   expect(compareFechaMasAntigua(act1, act2)).toBe(1) &&
     expect(compareFechaMasReciente(act1, act2)).toBe(2);
@@ -131,9 +120,10 @@ test('elimina una actividad de la base de datos', async () =>{
     await deleteActivity("Actividad lectura de libros a niños")
     expect(await getActivityById("Actividad lectura de libros a niños")).toBe(null)
 })
+
 test("seguir a una asociacion", async () => {
   const user = {
-    actividades: ["Apoyo Escolar a Jovenes de ESO", "Plantación en la Vallesa"],
+    actividades: ["Plantación en la Vallesa"],
     apellidos: "Cortés Reverón",
     correo: "sancore17@gmail.com",
     descripcion: "Voluntario con ganas de plantar arboles!!  🌱",
@@ -145,9 +135,9 @@ test("seguir a una asociacion", async () => {
     telefono: "640524839",
     puntos: 0,
   };
+  await followAsociation(user, "Modepran")
+  await sleep(1000)
   const asociacion = await getAsociationByID("Modepran");
-  expect(await followAsociation(user, "Modepran")).toBe(
-    asociacion.seguidores.includes(user.nombre + " " + user.apellidos)
-  );
+  expect(asociacion.seguidores.includes(user.nombre + " " + user.apellidos)).toBe(true)
 });
 
