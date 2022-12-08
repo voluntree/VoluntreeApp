8fe87345af3375972770533650f099df052168ad
adsomar <adrimiramar311@gmail.com>
2022-12-08 20:59:09 +0100

    Hora en los mensajes

--- components/chat/NotMyMessage.js
+++ components/chat/NotMyMessage.js
@@ -6,12 +6,18 @@ var width = Dimensions.get("window").width; //full width
 
 const NotMyMessage = (props) => {
   const { message } = props;
+  const hour = String(new Date(message.date).getHours()).padStart(2, '0') + ":" + String(new Date(message.date).getMinutes()).padStart(2, '0')
   return (
     <View className="flex mx-4 p-2 my-1 h-auto w-60 bg-costas rounded-lg">
       <Text className="font-semibold text-base text-ambiental">
         {message.user.nombre} {message.user.apellidos}
       </Text>
-      <Text className="flex-col flex-wrap text-base">{message.message}</Text>
+      <View className="flex-row flex-wrap rounded-lg justify-between">
+      <Text className="flex-col flex-wrap text-base text-ambiental">{message.message}</Text>
+      <View className = "items-end grow justify-end">
+          <Text className = "text-ambiental">{hour}</Text>
+        </View>
+      </View>
       <View style = {{position: "absolute",
                       top: 0,
                       left:-10,
