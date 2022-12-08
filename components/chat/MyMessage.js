3894879345bdf7c4fbbb7f4d5de22e4e5a5a05dc
adsomar <adrimiramar311@gmail.com>
2022-12-08 20:26:29 +0100

    Mensajes

--- components/chat/MyMessage.js
+++ components/chat/MyMessage.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Dimensions } from "react-native";
+import { theme } from "../../tailwind.config";
 
 var width = Dimensions.get('window').width; //full width
 
@@ -9,9 +10,19 @@ const MyMessage = (props) => {
   return (
     <View style={st.msg}>
       <View className="mx-4 p-2 my-1 h-auto w-52 bg-educacion rounded-lg">
-      
-      <Text className="flex-col flex-wrap text-base">{message.message}</Text>
-    </View>
+        <Text className="flex-col flex-wrap text-base">{message.message}</Text>
+      </View>
+      <View style = {{position: "absolute",
+                      top: 4,
+                      right:6,
+                      width:0, 
+                      height:0, 
+                      borderTopWidth: 25,
+                      borderTopColor: theme.colors.educacion,
+                      borderRightWidth: 25,
+                      borderRightColor: "transparent",
+                      zIndex: -1,
+                      borderRadius: 5}}/>
     </View>
   );
 };
--- components/chat/NotMyMessage.js
+++ components/chat/NotMyMessage.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Dimensions } from "react-native";
+import { theme } from "../../tailwind.config";
 var width = Dimensions.get("window").width; //full width
 
 const NotMyMessage = (props) => {
@@ -11,6 +12,17 @@ const NotMyMessage = (props) => {
         {message.user.nombre} {message.user.apellidos}
       </Text>
       <Text className="flex-col flex-wrap text-base">{message.message}</Text>
+      <View style = {{position: "absolute",
+                      top: 0,
+                      left:-10,
+                      width:0, 
+                      height:0, 
+                      borderTopWidth: 50,
+                      borderTopColor: theme.colors.costas,
+                      borderLeftWidth: 50,
+                      borderLeftColor: "transparent",
+                      zIndex: -1,
+                      borderRadius: 5}}/>
     </View>
   );
 };
