1e7fb5021b7cb66bf17c862f635180b6fd45271c
adsomar <adrimiramar311@gmail.com>
2022-12-09 20:01:39 +0100

    Noticias rediseño

--- components/articulos/TarjetaArticulo.js
+++ components/articulos/TarjetaArticulo.js
@@ -4,6 +4,7 @@ import { TouchableOpacity } from "react-native";
 import { Icon } from "react-native-elements";
 import { getAsociationByID, getAssociationByName, getImageDownloadURL } from "../../service/service";
 import { useNavigation } from "@react-navigation/native";
+import { theme } from "../../tailwind.config";
 
 const TarjetaArticulo = (props) => {
   const { articulo } = props;
@@ -34,19 +35,18 @@ const TarjetaArticulo = (props) => {
   return (
     <TouchableOpacity
       onPress={readArticle}
-      className="bg-[#ffffff] rounded-lg flex-col m-2"
+      className="mx-2"
     >
-      <View>
-        <Image className="h-40 w-max rounded-t-lg m-1" source={{ uri: uri }} />
-      </View>
-      <View className="px-3 pb-2">
-        <Text className="text-xl font-bold">{articulo.titulo}</Text>
-        <Text className="text-base">{articulo.introduccion}</Text>
-        <View className="flex-row justify-between items-center mt-2">
+      <View className = "absolute h-full w-0.5 bg-ambiental top-0 left-7"></View>
+      <View className = "flex p-2">
+        {/* Asociacion y fecha publicación */}
+        
+        <View className = "flex-row justify-between items-center">
+        <View className="flex-row justify-between items-center">
           <View className="flex-row">
             {asociacion != undefined ? (
               <Image
-                className="rounded-sm h-10 w-10 mr-2"
+                className="rounded-full h-10 w-10 mr-2"
                 source={{ uri: uri2 }}
               />
             ) : (
@@ -54,22 +54,30 @@ const TarjetaArticulo = (props) => {
             )}
             <View className="flex-col">
               {asociacion != undefined ? (
-                <Text>{asociacion.nombre}</Text>
+                <Text className = "text-ambiental font-bold">{asociacion.nombre}</Text>
               ) : (
                 <></>
               )}
-              <Text>{articulo.fecha_publicacion}</Text>
+              <Text className = "text-ambiental">{articulo.fecha_publicacion}</Text>
             </View>
           </View>
-
-          <View className="flex-row items-center">
-            <Text>{articulo.favoritos.length}</Text>
-            <TouchableOpacity className="ml-2">
-              <Icon name="heart" type="octicon" size={28} />
-            </TouchableOpacity>
+        </View>
+        <View className="flex-row items-center justify-start">
+              <Text className = "text-ambiental">{articulo.favoritos.length}</Text>
+              <TouchableOpacity className="ml-2">
+                <Icon name="heart" type="octicon" size={18} color={theme.colors.ambiental}/>
+              </TouchableOpacity>
+            </View>
+        </View>
+        <View className = "flex  justify-center items-center">
+          <View className = "w-full pl-12">
+            <Image className="h-40 w-full rounded-md" source={{ uri: uri }} />
+            <Text className="text-base text-ambiental font-bold">{articulo.titulo}</Text>
+            <Text className="text-sm text-ambiental">{articulo.introduccion}</Text>
           </View>
         </View>
       </View>
+      
     </TouchableOpacity>
   );
 };
